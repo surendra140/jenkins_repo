cf806f2334d8ac3ebcecadb3110e1ef905aee669
Surendra <surendrabankar218@gmail.com>
2022-07-05 12:06:45 +0530

    adding demo mvn project

---
+++ demo1/src/main/java/com/wipro/demo/App.java
@@ -0,0 +1,13 @@
+package com.wipro.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
